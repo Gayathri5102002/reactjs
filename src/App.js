797d7e3f6e15ed19bc170ec63fc78d0198f2df90
Gayathri5102002 <gayu05102002@gmail.com>
2024-01-30 23:21:30 +0530

    feature signup working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
+import News from './component/News';
+import { useEffect, useState } from 'react';
+import "./component/News.css"
+
 function App() {
+
+  let [articles, setarticles] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://newsapi.org/v2/everything?q=india&from=2023-12-09&apiKey=9263bdb8eeb740dbba5b973e9b907dfc`)
+      .then((response) => response.json())
+      .then((news) => {
+        setarticles(news.articles);
+        console.log(news.articles)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [])
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+        <h1>Parso Tak</h1>
+        <input type="text" placeholder='Search News'></input>
       </header>
+
+      <section className="newsarticle">
+
+      {articles.map((article) => {
+          return (
+            <News article={article} />
+
+          )
+        })}
+
+
+
+      </section>
     </div>
+
   );
 }
+
 
 export default App;
